f9f4941e9f30e6e0ba2e8ede303d398e00ac07c1
yongheuicho <yongheuicho@gmail.com>
2018-05-15 09:49:32 +0900

    [2018-05-15] DogLove #1

---
+++ mypush.bat
@@ -0,0 +1,3 @@
+git add --all
+git commit -m "[2018-05-15] DogLove #1"
+git push -u origin master
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <section class="section">
+    <div class="content">
+    <h1 class="title is-1">개사랑</h1>
+    <h2 class="subtitle">개사랑 사이트는 개를 사랑하는 사람들을 위한 상세한 정보를 제공합니다.</h2>  
+    </div>
+  </section>
+</template>
+
+<script>
+
+</script>
