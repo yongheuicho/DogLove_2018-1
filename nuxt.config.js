8a9f70491b554b70bdc39ceb37680424e6f56be6
yongheuicho <yongheuicho@gmail.com>
2018-05-15 11:32:05 +0900

    [2018-05-15] DogLove #8

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,11 +3,11 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: "doglove",
+    title: "개사랑",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "Nuxt.js project" }
+      { hid: "description", name: "description", content: "대한민국 애견인의 개사랑" }
     ],
     link: [
       { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
