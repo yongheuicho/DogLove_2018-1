da9a207f3452cbc0f2d4dc3c9052839096260b89
yongheuicho <yongheuicho@gmail.com>
2018-05-29 12:33:34 +0900

    [2018-05-29] DogLove #16

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
     <nuxt/>
+    <footer class="footer has-text-centered">
+      <p>2018년 목원대학교 개사랑. 모든 저작권은 개사랑에 있습니다.</p>
+    </footer>
   </div>
 </template>
--- mypush.bat
+++ mypush.bat
@@ -1,3 +1,3 @@
 git add --all
-git commit -m "[2018-05-29] DogLove #15"
+git commit -m "[2018-05-29] DogLove #16"
 git push -u origin master
