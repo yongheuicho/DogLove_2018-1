63289f83be73ac4615e5186e49b8d4a34daced83
yongheuicho <yongheuicho@gmail.com>
2018-05-29 12:46:55 +0900

    [2018-05-29] DogLove #17

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,15 @@
 <template>
   <div>
+    <nav class="navbar">
+      <div class="navbar-brand">
+        <nuxt-link to="/" class="navbar-item">개사랑</nuxt-link>
+      </div>
+      <div class="navbar-menu is-active">
+        <div class="navbar-end">
+              <nuxt-link to="/about" class="navbar-item">소개</nuxt-link>
+        </div>
+      </div>
+    </nav>
     <nuxt/>
     <footer class="footer has-text-centered">
       <p>2018년 목원대학교 개사랑. 모든 저작권은 개사랑에 있습니다.</p>
---
+++ pages/about.vue
@@ -0,0 +1,9 @@
+<template>
+    <section class="section">
+        <article class="hero is-info">
+            <div class="hero-body">
+                <h1 class="title">개사랑을 소개합니다.</h1>
+            </div>
+        </article>
+    </section>
+</template>
