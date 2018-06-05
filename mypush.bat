4c3b2b749ba223cccd82d9a7362b4e9de1a69dca
yongheuicho <yongheuicho@gmail.com>
2018-06-05 10:35:39 +0900

    [2018-06-05] DogLove #22

--- mypush.bat
+++ mypush.bat
@@ -1,3 +1,3 @@
 git add --all
-git commit -m "[2018-06-05] DogLove #21"
+git commit -m "[2018-06-05] DogLove #22"
 git push -u origin master
--- plugins/myBarChart.js
+++ plugins/myBarChart.js
@@ -1,13 +1,10 @@
 export default function(elementName) {
     var container = document.getElementById(elementName);
     var data = {
-      categories: ['June', 'July', 'Aug', 'Sep', 'Oct', 'Nov'],
+      categories: ['2012', '2014', '2016', '2018', '2018'],
       series: [{
-        name: 'Budget',
-        data: [5000, 3000, 5000, 7000, 6000, 4000]
-      },{
-        name: 'Income',
-        data: [8000, 1000, 7000, 2000, 5000, 3000]
+        name: '시장 규모 (억원)',
+        data: [9000, 14000, 23000, 37000, 58000]
       }]
     };
     tui.chart.barChart(container, data);
