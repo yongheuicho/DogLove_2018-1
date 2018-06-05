3a535212b9395bbd868114607b59eedb971bed95
yongheuicho <yongheuicho@gmail.com>
2018-06-05 09:37:08 +0900

    [2018-06-05] DogLove #18

--- mypush.bat
+++ mypush.bat
@@ -1,3 +1,3 @@
 git add --all
-git commit -m "[2018-05-29] DogLove #17"
+git commit -m "[2018-06-05] DogLove #18"
 git push -u origin master
--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,11 @@ module.exports = {
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "대한민국 애견인의 개사랑" }
+      {
+        hid: "description",
+        name: "description",
+        content: "대한민국 애견인의 개사랑"
+      }
     ],
     link: [
       { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
@@ -15,13 +19,25 @@ module.exports = {
         rel: "stylesheet",
         href:
           "https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css"
+      },
+      {
+        rel: "stylesheet",
+        href: "https://uicdn.toast.com/tui.chart/latest/tui-chart.css"
       }
     ],
     script: [
       {
         src: "https://use.fontawesome.com/releases/v5.0.7/js/all.js",
         defer: ""
-      }
+      },
+      {
+        src:
+          "https://uicdn.toast.com/tui.code-snippet/latest/tui-code-snippet.js"
+      },
+      {
+        src: "https://cdnjs.cloudflare.com/ajax/libs/raphael/2.2.7/raphael.js"
+      },
+      { src: "https://uicdn.toast.com/tui.chart/latest/tui-chart.js" }
     ]
   },
   /*
@@ -32,7 +48,7 @@ module.exports = {
   ** Build configuration
   */
   build: {
-    vendor: ['axios'],
+    vendor: ["axios"],
     /*
     ** Run ESLint on save
     */
